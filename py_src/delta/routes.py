79255842a40768fa3c07d30d513f185ceabe5ea4
aston-evans <findaston@gmail.com>
2025-08-29 21:12:41 -0400

    added routes to the 4 pages on navbar

--- py_src/delta/routes.py
+++ py_src/delta/routes.py
@@ -1,11 +1,31 @@
 from fastapi import APIRouter, Depends, Form, HTTPException, Request  # noqa: F401
-from fastapi.responses import HTMLResponse, RedirectResponse # noqa: F401
+from fastapi.responses import HTMLResponse, RedirectResponse  # noqa: F401
 from delta.db import templates
 
 router = APIRouter()
 
+
 @router.get("/", response_class=HTMLResponse)
 async def root(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
+
+@router.get("/home", response_class=HTMLResponse)
+async def home(request: Request):
     return templates.TemplateResponse("home.html", {
         "request": request
     })
+
+
+@router.get("/clearance", response_class=HTMLResponse)
+async def clearance(request: Request):
+    return templates.TemplateResponse("clearance.html", {"request": request})
+
+
+@router.get("/about", response_class=HTMLResponse)
+async def about(request: Request):
+    return templates.TemplateResponse("about.html", {"request": request})
+
+
+@router.get("/contact", response_class=HTMLResponse)
+async def contact(request: Request):
+    return templates.TemplateResponse("contact.html", {"request": request})
